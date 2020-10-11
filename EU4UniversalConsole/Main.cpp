37cb226ff15eab4c23f29d316ef5b9dcd495072a
Taikelenn <taikelenn@kasumata.ee>
2020-10-12 01:39:34 +0300

    Added all Visual Studio related project files

---
+++ EU4UniversalConsole/Main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		// placeholder
+	}
+
+	return TRUE;
+}
